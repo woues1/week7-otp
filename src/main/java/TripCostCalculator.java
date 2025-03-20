ea5f6d7eea72b3693cddc24a2ae71e56fa28e0f4
woues1 <toni.hirvikallio@gmail.com>
2025-03-20 10:53:53 +0200

    fix?

--- src/main/java/TripCostCalculator.java
+++ src/main/java/TripCostCalculator.java
@@ -1,3 +1,6 @@
+import java.text.MessageFormat;
+import java.util.Locale;
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
 public class TripCostCalculator {
@@ -8,13 +11,43 @@ public class TripCostCalculator {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the distance in km: ");
-        double km = sc.nextDouble();
-        System.out.println("Enter the fuel price: ");
-        double fuelPrice = sc.nextDouble();
-        System.out.println("Enter the fuel consumption per 100 km: ");
-        double fuelConsumption = sc.nextDouble();
-        System.out.println("The cost of the trip is: " + calculateTripCost(km, fuelPrice, fuelConsumption));
+        System.out.println("Select" +
+                " language: 1 - farsi, " +
+                "2 - Japanese" +
+                "3 - Finnish");
+        Locale locale;
+        int lang = sc.nextInt();
+        switch (lang){
+            case 1:
+                locale = new Locale("fa", "IR");
+                break;
+            case 2:
+                locale = new Locale("ja", "JP");
+                break;
+            case 3:
+                locale = new Locale("fi", "FI");
+            default:
+                locale = new Locale("fi", "FI");
+        }
 
+        ResourceBundle rb = null;
+        try{
+            rb = ResourceBundle.getBundle("messages", locale);
+        }
+        catch(Exception e){
+            System.out.println("invalid selection");
+            ResourceBundle.getBundle("messages", new Locale("fi", "FI"));
+        }
+
+
+        System.out.println(rb.getString("kilo"));
+        double km = sc.nextDouble();
+        System.out.println(rb.getString("price"));
+        double fuelPrice = sc.nextDouble();
+        System.out.println(rb.getString("price_KM"));
+        double fuelConsumption = sc.nextDouble();
+        String total = MessageFormat.format(rb.getString("total"), calculateTripCost(km, fuelPrice, fuelConsumption));
+        System.out.println(total);
+        //System.out.println("The cost of the trip is: " + calculateTripCost(km, fuelPrice, fuelConsumption));
     }
 }
